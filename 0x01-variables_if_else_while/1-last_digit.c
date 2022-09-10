47b6d3c992e15b41bb3598448e7191e1c23220b7
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-09-10 01:08:38 +0100

    fix last digit int error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
         }
         else if (digit < 6 && digit != 0)
         {
-                printf("Last digit of %i is %i and is less than 6 and not 0\n");
+                printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
         }
 	return (0);
 }
