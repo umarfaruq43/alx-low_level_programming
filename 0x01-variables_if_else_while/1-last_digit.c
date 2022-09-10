2616005c62dfe3e2ed6f04fc377bee59f3b3d32a
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-09-10 01:21:08 +0100

    fix last digit int error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,26 +13,20 @@
 
 int main(void)
 {
-        int n;
-        int digit;
+        int n, digit;
 
         srand(time(0));
         n = rand() - RAND_MAX / 2;
-        /* your code goes there */
 
-        digit = n % 10; /* This code  is to get the last digit*/
+        /*my code*/
+        digit = n % 10; /*gets last digit*/
 
         if (digit > 5)
-        {
                 printf("Last digit of %i is %i and is greater than 5\n", n, digit);
-        }
         else if (digit == 0)
-        {
                 printf("Last digit of %i is %i and is 0\n", n, digit);
-        }
         else if (digit < 6 && digit != 0)
-        {
                 printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
-        }
+
         return (0);
 }
