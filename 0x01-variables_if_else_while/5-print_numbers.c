9f3b76e94b12009e11c44e95f72ba855f9363eb5
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-09-12 03:24:57 +0100

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * return: Always 0 success
+ */
+int main(void)
+{
+	int start = 0;
+
+	while (start < 10)
+	{
+		printf("%i\n", start);
+		start++;
+	}
+	return (0);
+}
