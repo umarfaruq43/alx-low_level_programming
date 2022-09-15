0811b9448dfe9d38bba760afec89c7d7516d1b2f
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-09-15 14:41:30 +0100

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper -> Enrty point
+ *
+ * Description: checking if c is lower or upper case.
+ * @c: an input cahracter.
+ * Return: (1) if c is uppercase and (0) if is Lowercase.
+ *
+ */
+int _isupper(int c)
+{
+	if (_isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
