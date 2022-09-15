a87b500209b368e54959277e1b6b1025eaab1908
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-09-15 03:27:55 +0100

    update

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 fib
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	long a = 1, b = 2;
+	long sum = a + b;
+
+	printf("%ld, %ld, ", a, b);
+
+	for (i = 3; i < 50; ++i)
+	{
+		printf("%ld, ", sum);
+		a = b;
+		b = sum;
+		sum = a + b;
+	}
+	if (i == 50)
+	{
+		printf("%ld ", sum);
+	}
+	return (0);
+}
