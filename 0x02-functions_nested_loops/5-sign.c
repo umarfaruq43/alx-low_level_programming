a3607e203a9beb2ec205462ffae9198f85429fbb
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-09-15 03:02:44 +0100

    update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - checks if c is lowercase
+ *
+ * @n: num to be checked
+ *
+ * Return: A1 if lowercase, 0 if uppercase
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
