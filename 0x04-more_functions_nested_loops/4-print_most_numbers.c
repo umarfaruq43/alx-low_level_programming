a3d2ffcb8382e15e312bf597ad8e8247a60ac7e3
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-09-16 22:57:26 +0100

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry Point
+ * Description:
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		if (a != '2' && a != '4')
+		{
+
+			_putchar(a);
+		}
+
+		a++;
+	}
+	_putchar('\n');
+}
