43995fe45267293c34d90d165d5494367ffde065
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-09-17 00:39:58 +0100

    update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ * Description: printing more numbers using nested loops
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int a = 0;
+	int b;
+
+	while (a <= 10)
+	{
+		b = 0;
+
+		while (b <= '9')
+		{
+			// _putchar(b);
+		}
+		b++;
+		a++;
+
+		_putchar('\n');
+	}
+}
