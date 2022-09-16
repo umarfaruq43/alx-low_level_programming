045dc1cf6834fd33cd8d4d08e465862629cdfe00
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-09-16 22:09:26 +0100

    Mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
 /**
+ * mul - Entry point
  * @a: input character
- *
+ * @b: input character
+ * Description: Mutipule of two numbers
+ * Return: Always 0 Success
  */
 
 int mul(int a, int b)
 {
-	
+	return (a * b);
 }
