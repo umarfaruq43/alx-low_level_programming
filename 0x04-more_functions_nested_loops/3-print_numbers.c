24d1bc2b76081e9651e3e98b2c15bbef0b0899ef
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-09-16 22:37:33 +0100

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ * Description: print int 0 - 9 with putchar
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar("\n");
+}
